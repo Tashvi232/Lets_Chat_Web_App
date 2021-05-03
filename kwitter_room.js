ca26820715297e5a09a3e08bc328c6494971d50f
Tashvi232 <79177478+Tashvi232@users.noreply.github.com>
2021-05-03 16:08:10 +0800

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCJXO4dKTHEZ1h0TqihC5fK8WZxQfLGsz0",
+    authDomain: "lets-chat-web-app-2d03c.firebaseapp.com",
+    projectId: "lets-chat-web-app-2d03c",
+    storageBucket: "lets-chat-web-app-2d03c.appspot.com",
+    messagingSenderId: "562878943193",
+    appId: "1:562878943193:web:591ea18684d522f75893c8"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
